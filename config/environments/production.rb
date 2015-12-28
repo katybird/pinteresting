689c5d9b81bc9f993b56d6d4f9a3848cdc73e4a7
Katy Beavers <katybeavers@gmail.com>
2015-12-27 19:26:11 -0600

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  #note to set to production host
+  config.action_mailer.default_url_options = { :host => 'jbirdstreet.herokuapp.com' }
+
 end
